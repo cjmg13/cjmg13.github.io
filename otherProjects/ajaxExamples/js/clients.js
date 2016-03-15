735936cbce748eb0f733d117e93cf2795becbfe0
ganasecory151@gmail.com <ganasecory151@gmail.com>
2016-03-15 12:28:07 -0400

    update ajax
    ajax

--- otherProjects/ajaxExamples/js/clients.js
+++ otherProjects/ajaxExamples/js/clients.js
@@ -1,41 +1,45 @@
-$("#getClients").on("click", function() {
+$(document).ready(function() {
 
-  alert("working");
+    $("#getClients").on("click", function() {
+      //when u click on the button this function is called
 
-})
+      var url = "http://brookesDaphne.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json"
+      $.getJSON(url, function(data) {
 
+        //table format
+        //<table>
+        //<tr> <th></th> <th></th> </tr>
+        //<tr> <td></td> <td></td> </tr>
+        //<table>
 
+        var html = "<table class='table table-hover table-striped'>"+
+          "<tr> <th>Name</th> <th>Email</th> <th>Company</th> </tr>";
 
+        $.each(data, function(index, item) {
 
+          //$("#data").append(item.name);
 
+html+= "<tr>"+
+"<td>" + item.name + "</td>" +
+"<td>" + item.email + "</td>" +
+"<td>"+item.company + "</td>" +
+"</tr>";
 
+        })
 
+        html += "</table>";
 
+//puts all html into the div
+        $("#data").append(html);
 
-/*
+        //alert(data;)
+        //dir makes it readable for humans in the console
+        //  console.dir(data);
 
-$("#getClients").click(function() {
-  //will not work locally with Chrome (use Safari, FF)
-  $.getJSON("jsonDatabase/clients.json", function(data) {
+        //end json
+      })
 
-    var table = "<table class='table table-striped table-hover'>" +
-      "<tr><th>Name</th><th>Company</th><th>Email</th><th>Phone</th><th>Balance</th></tr>";
+      //end click
+    })
 
-    $.each(data, function(index, item) {
-      table += "<tr>"+
-      "<td>" + item.name + "</td>" +
-        "<td>" + item.company + "</td>" +
-        "<td>" + item.email + "</td>" +
-        "<td>" + item.phone + "</td>" +
-        "<td>" + item.balance + "</td>"+
-        "</tr>";
-    });
-
-    table += "</table>";
-    $("#data").append(table);
-
-  });//getJSON
-});//click
-
-//to look at a JSON file clearly copy teh contents of clients.json into http://www.jsoneditoronline.org/
-*/
+  }) //document ready
