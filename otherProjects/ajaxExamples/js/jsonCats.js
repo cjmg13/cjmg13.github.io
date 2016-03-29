98eec4282bd55013561e2d4140b34f1f38e93f38
ganasecory151@gmail.com <ganasecory151@gmail.com>
2016-03-29 07:59:51 -0400

    updated folder
    folder changed

--- otherProjects/ajaxExamples/js/jsonCats.js
+++ otherProjects/ajaxExamples/js/jsonCats.js
@@ -1,1 +1,37 @@
-/**/
+$(document).ready(function() {
+
+    $.getJSON("jsonDatabase/cat.json", function(data) {
+
+      console.dir(data);
+      var html = "";
+
+      $.each(data, function(index, item) {
+          html += '<div class="col-md-4">' +
+            '<div class="catName">' + item.name + '</div>' +
+            '<div class="catType">' + item.type + '</div>' +
+            '<div class="catGender">' + item.gender + '</div>' +
+            '<img src="' + item.image + '"/>';
+          
+          html += '</div>';
+        }) //each cat
+
+      $("#catData").append(html);
+    })
+  })
+  /*
+  //one per cat
+  <div class="col-md-4 cat">
+    <div class="catName"></div>
+    <div class="catType"></div>
+    <div class="catGender"></div>
+    <img src=""/>
+    <div class="commentsContainer">
+      //one per comment
+      <div class="renterName"></div>
+      <div class="renterComment"></div>
+      <div class="renterStars">
+      //5 stars, some full, some empty
+      </div> //end starts
+    </div>//end commentsContainer
+  </div>//end cat
+  */
